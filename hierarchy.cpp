0b7f2ce814ba8bc4ce36e35291212599bbb9c22e
Tom Lagier <tom@gliffy.com>
2017-05-28 22:57:10 -0700

    Huge memory leak somewhere, but we're making progress. Successful on 4 nodes

--- hierarchy.cpp
+++ hierarchy.cpp
@@ -8,11 +8,22 @@
 
 using namespace emscripten;
 
-class HNode {
+class HBasis {
+    public:
+      HBasis(){};
+      HBasis(double x, double y, double r)
+          : x(x), y(y), r(r){};
+
+      double x = 0;
+      double y = 0;
+      double r = 0;
+};
+
+class HNode: public HBasis {
     public:
         HNode(emscripten::val node)
         {
-            value = node["value"].as<float>();
+            value = node["value"].as<double>();
 
             if(node["children"].typeof().as<std::string>() == "object") {
                 std::vector<emscripten::val> children_ = emscripten::vecFromJSArray<emscripten::val>(node["children"]);
@@ -30,10 +41,7 @@ class HNode {
             parent = parent_;
         }
 
-        float value = 0;
-        float x = 0;
-        float y = 0;
-        float r = 0;
+        double value = 0;
 
         std::vector<HNode *> children;
         HNode* parent;
@@ -86,26 +94,21 @@ class HNode {
 
             return *this;
         };
-
-        // void doSomeStuff() {
-        //     std::cout << parent->value << std::endl;
-        // }
 };
 
 void place(HNode* a, HNode* b, HNode* c) {
-    float ax = a->x;
-    float ay = a->y;
-    float da = b->r + c->r;
-    float db = a->r + c->r;
-    float dx = b->x - ax;
-    float dy = b->y - ay;
-    float dc = dx * dx + dy * dy;
-    float db_ = db - dc; //Might need to revert this to match place
+    double ax = a->x;
+    double ay = a->y;
+    double da = b->r + c->r;
+    double db = a->r + c->r;
+    double dx = b->x - ax;
+    double dy = b->y - ay;
+    double dc = dx * dx + dy * dy;
 
     if(dc > 0) {
-        float x = 0.5 + ((db *= db) - (da *= da)) / (2 * dc);
-        float y = sqrt(fmax(0, 2 * da * (db + dc) - db_ * db_ - da * da)) / (2 * dc);
-        c->x = ax + x * dy + y * dy;
+        double x = 0.5 + ((db *= db) - (da *= da)) / (2 * dc);
+        double y = sqrt(fmax(0, 2 * da * (db + dc) - (db -= dc) * db - da * da)) / (2 * dc);
+        c->x = ax + x * dx + y * dy;
         c->y = ay + x * dy - y * dx;
     } else {
         c->x = ax + db;
@@ -124,30 +127,167 @@ struct HLNode {
 };
 
 bool intersects(HNode* a, HNode* b) {
-    float dx = b->x - a->x;
-    float dy = b->y - a->y;
-    float dr = a->r + b->r;
+    double dx = b->x - a->x;
+    double dy = b->y - a->y;
+    double dr = a->r + b->r;
     return dr * dr - 1e-6 > dx * dx + dy * dy;
 }
 
-float distance2(HLNode* node, float x, float y) {
+double distance2(const HLNode* node, double x, double y) {
     HNode *a = node->_;
     HNode *b = node->next->_;
-    float ab = a->r + b->r;
-    float dx = (a->x * b->r + b->x * a->r) / ab - x;
-    float dy = (a->y * b->r + b->y * a->r) / ab - y;
+    double ab = a->r + b->r;
+    double dx = (a->x * b->r + b->x * a->r) / ab - x;
+    double dy = (a->y * b->r + b->y * a->r) / ab - y;
     return dx * dx + dy * dy;
 }
 
-HNode* enclose(std::vector<HNode*> nodes) {
-    return nodes[0];
+bool encloses(HBasis *a, HBasis* b) {
+    double dr = a->r - b->r + 1e-9;
+    double dx = b->x - a->x;
+    double dy = b->y - a->y;
+    return dr > 0 && dr * dr > dx * dx + dy * dy;
 }
 
-float packEnclose(std::vector<HNode *> circles)
+HBasis* encloseBasis1(HBasis* a) {
+    return new HBasis(a->x, a->y, a->r);
+}
+
+HBasis* encloseBasis2(HBasis* a, HBasis* b) {
+    double x1 = a->x, y1 = a->y, r1 = a->r;
+    double x2 = b->x, y2 = b->y, r2 = b->r;
+    double x21 = x2 - x1, y21 = y2 - y1, r21 = r2 - r1;
+    double l = sqrt(x21 * x21 + y21 * y21);
+    return new HBasis(
+        (x1 + x2 + x21 / l * r21) / 2,
+        (y1 + y2 + y21 / l * r21) / 2,
+        (l + r1 + r2) / 2
+    );
+}
+
+HBasis* encloseBasis3(HBasis* a, HBasis* b, HBasis* c) {
+    double x1 = a->x, y1 = a->y, r1 = a->r,
+      x2 = b->x, y2 = b->y, r2 = b->r,
+      x3 = c->x, y3 = c->y, r3 = c->r,
+      a2 = x1 - x2,
+      a3 = x1 - x3,
+      b2 = y1 - y2,
+      b3 = y1 - y3,
+      c2 = r2 - r1,
+      c3 = r3 - r1,
+      d1 = x1 * x1 + y1 * y1 - r1 * r1,
+      d2 = d1 - x2 * x2 - y2 * y2 + r2 * r2,
+      d3 = d1 - x3 * x3 - y3 * y3 + r3 * r3,
+      ab = a3 * b2 - a2 * b3,
+      xa = (b2 * d3 - b3 * d2) / (ab * 2) - x1,
+      xb = (b3 * c2 - b2 * c3) / ab,
+      ya = (a3 * d2 - a2 * d3) / (ab * 2) - y1,
+      yb = (a2 * c3 - a3 * c2) / ab,
+      A = xb * xb + yb * yb - 1,
+      B = 2 * (r1 + xa * xb + ya * yb),
+      C = xa * xa + ya * ya - r1 * r1,
+      r = A ? (B + sqrt(B * B - 4 * A * C)) / (2 * A) : C / B,
+      x = x1 + xa - xb * r,
+      y = y1 + ya - yb * r;
+
+  return new HBasis(
+    x, y,
+    fmax(fmax(
+        sqrt((x1 -= x) * x1 + (y1 -= y) * y1) + r1,
+        sqrt((x2 -= x) * x2 + (y2 -= y) * y2) + r2),
+        sqrt((x3 -= x) * x3 + (y3 -= y) * y3) + r3)
+    );
+}
+
+bool isBasis3(HBasis* a, HBasis* b, HBasis* c) {
+    bool ret = !encloses(encloseBasis2(a, b), c)
+      && !encloses(encloseBasis2(a, c), b)
+      && !encloses(encloseBasis2(b, c), a);
+
+    return ret;
+}
+
+HBasis* encloseBasis(std::vector<HBasis *> B){
+    switch(B.size()) {
+        case 1:
+            return encloseBasis1(B[0]);
+        case 2:
+            return encloseBasis2(B[0], B[1]);
+        case 3:
+            return encloseBasis3(B[0], B[1], B[2]);
+        default:
+            return NULL;
+        }
+};
+
+bool enclosesAll(HBasis* a, std::vector<HBasis*> B) {
+    for (int i = 0; i < B.size(); ++i) {
+        if (!encloses(a, B[i])) {
+            return false;
+        }
+    }
+  return true;
+}
+
+std::vector<HBasis *> extendBasis(std::vector<HBasis *> B, HBasis* p) {
+    std::vector<HBasis *> rvec = std::vector<HBasis *>();
+    int i;
+    int j;
+
+    if (enclosesAll(p, B))
+    {
+        rvec.push_back(p);
+        return rvec;
+    }
+
+    for (i = 0; i < B.size(); ++i) {
+
+        if (enclosesAll(encloseBasis2(B[i], p), B))
+        {
+            rvec.push_back(B[i]);
+            rvec.push_back(p);
+            return rvec;
+        }
+    }
+
+    for (i = 0; i < B.size() - 1; ++i) {
+        for (j = i + 1; j < B.size(); ++j) {
+            if (isBasis3(B[i], B[j], p) && enclosesAll(encloseBasis3(B[i], B[j], p), B)) {
+                rvec.push_back(B[i]);
+                rvec.push_back(B[j]);
+                rvec.push_back(p);
+                return rvec;
+            }
+        }
+    }
+}
+
+std::vector<HBasis*> findBasis(std::vector<HBasis *> L, int n, std::vector<HBasis *> B){
+    HBasis *circle = encloseBasis(B);
+
+    for (int i = 0; i < n; ++i) {
+        HBasis *p = L[i];
+        if (circle == NULL || !encloses(circle, p))
+        {
+            circle = encloseBasis(B = findBasis(L, i + 1, extendBasis(B, p)));
+        }
+    }
+
+    return B;
+};
+
+HBasis* enclose(std::vector<HBasis*> circles) {
+    return encloseBasis(findBasis(circles, circles.size(), std::vector<HBasis*>()));
+}
+
+double packEnclose(std::vector<HNode *> circles)
 {
     int n = circles.size();
 
-    if(n == 0) {
+    n = circles.size();
+
+    if (n == 0)
+    {
         return 0;
     }
 
@@ -175,39 +315,42 @@ float packEnclose(std::vector<HNode *> circles)
     c = circles[2];
     place(b, a, c);
 
-    float aa = a->r * a->r;
-    float ba = b->r * b->r;
-    float ca = c->r * c->r;
-    float oa = aa + ba + ca;
-    float ox = aa * a->x + ba * b->x + ca * c->x;
-    float oy = aa * a->y + ba * b->y + ca * c->y;
-    float cx;
-    float cy;
+    double aa = a->r * a->r, ba = b->r * b->r, ca = c->r * c->r;
+    double oa = aa + ba + ca;
+    double ox = aa * a->x + ba * b->x + ca * c->x;
+    double oy = aa * a->y + ba * b->y + ca * c->y;
+    double cx, cy, sj, sk;
     int i;
     HLNode* j;
     HLNode* k;
-    float sj;
-    float sk;
     bool kill;
 
     HLNode *a_ = new HLNode(a);
     HLNode *b_ = new HLNode(b);
-    HLNode *c_;
+    HLNode *c_ = new HLNode(c);
+
+    a_->next = c_->previous = b_;
+    b_->next = a_->previous = c_;
+    c_->next = b_->previous = a_;
+
+    HLNode *d_;
 
     for (i = 3; i < n; ++i) {
-        c = circles[i];
-        place(a_->_, b_->_, c);
-        c_ = new HLNode(c);
+        place(a_->_, b_->_, c = circles[i]);
+        delete d_;
+        HLNode *d_ = new HLNode(c);
+
         j = b_->next;
         k = a_->previous;
         sj = b_->_->r;
         sk = a_->_->r;
         kill = false;
+
         do
         {
             if (sj <= sk)
             {
-                if(intersects(j->_, c_->_)) {
+                if(intersects(j->_, d_->_)) {
                     b_ = j;
                     a_->next = b_;
                     b_->previous = a_;
@@ -218,7 +361,7 @@ float packEnclose(std::vector<HNode *> circles)
                     j = j->next;
                 }
             } else {
-                if(intersects(k->_, c_->_)) {
+                if(intersects(k->_, d_->_)) {
                     a_ = k;
                     a_->next = b_;
                     b_->previous = a_;
@@ -239,9 +382,12 @@ float packEnclose(std::vector<HNode *> circles)
             continue;
         }
 
+        c_ = d_;
+        delete d_;
         c_->previous = a_;
         c_->next = b_;
-        c_ = b_;
+        b_ = c_;
+        b_->previous = b_;
         a_->next = b_->previous;
 
         oa += ca = c_->_->r * c_->_->r;
@@ -250,33 +396,34 @@ float packEnclose(std::vector<HNode *> circles)
 
         aa = distance2(a_, cx = ox / oa, cy = oy / oa);
         while((c_ = c_->next) != b_) {
-            if((ca = distance2(c_, cx, cy)) < aa) {
-                a_ = c_;
-                aa = ca;
+            if ((ca = distance2(c_, cx, cy)) < aa)
+            {
+                a_ = c_, aa = ca;
             }
         }
         b_ = a_->next;
     }
 
-    std::vector<HNode *> avec = std::vector<HNode *>();
+    std::vector<HBasis *> avec = std::vector<HBasis *>();
     avec.push_back(b_->_);
     c_ = b_;
     while((c_ = c_->next)!= b_) {
         avec.push_back(c_->_);
     }
-    c = enclose(avec);
+
+    HBasis *basis = enclose(avec);
 
     for (i = 0; i < n; ++i) {
         a = circles[i];
-        a->x -= c->x;
-        a->y -= c->y;
+        a->x -= basis->x;
+        a->y -= basis->y;
     }
 
-    return c->r;
+    return basis->r;
 }
 
 std::function<void(HNode*)> radiusLeaf(
-    std::function<float(HNode*)> radius){
+    std::function<double(HNode*)> radius){
     return [radius](HNode* node) {
         if(node->children.size() <= 0)
         {
@@ -286,22 +433,22 @@ std::function<void(HNode*)> radiusLeaf(
 };
 
 std::function<void(HNode *)> packChildren(
-    std::function<float(HNode *)> padding,
-    float k){
+    std::function<double(HNode *)> padding,
+    double k){
     return [padding, k](HNode *node) {
         if(!node->children.empty())
         {
             std::vector<HNode *> children = node->children;
             int i = 0;
             int n = children.size();
-            float r = fmax(padding(node) * k, 0);
+            double r = fmax(padding(node) * k, 0);
             if (r > 0)
             {
                 for (i = 0; i < n; ++i) {
                     children[i]->r += r;
                 }
             }
-            float e = packEnclose(children);
+            double e = packEnclose(children);
             if (r > 0)
             {
                 for (i = 0; i < n; ++i) {
@@ -313,11 +460,14 @@ std::function<void(HNode *)> packChildren(
     };
 };
 
-std::function<void(HNode *)> translateChild(float k) {
+std::function<void(HNode *)> translateChild(double k) {
     return [k](HNode *node) {
         HNode *parent = node->parent;
+
         node->r *= k;
-        if(parent != NULL) {
+
+        if (parent != NULL)
+        {
             node->x = parent->x + k * node->x;
             node->y = parent->y + k * node->y;
         }
@@ -335,9 +485,9 @@ class Hierarchy {
         return (value.typeof().as<std::string>() == type);
     }
 
-    float dx;
-    float dy;
-    float padding = 0;
+    double dx = 1;
+    double dy = 1;
+    double padding = 0;
     emscripten::val options;
     HNode *root;
 
@@ -346,37 +496,37 @@ class Hierarchy {
 
         if (this->isType(options["size"], "object"))
         {
-            dx = options["size"][0].as<float>();
-            dy = options["size"][1].as<float>();
+            dx = options["size"][0].as<double>();
+            dy = options["size"][1].as<double>();
         }
 
         if(this->isType(options["padding"], "number"))
         {
-            padding = options["padding"].as<float>();
+            padding = options["padding"].as<double>();
         }
 
         root->x = dx / 2;
         root->y = dy / 2;
-        std::function<float(HNode *)> radiusFunction = [this](HNode *node) {
+        std::function<double(HNode *)> radiusFunction = [this](HNode *node) {
             if(this->isType(options["radius"], "function")) {
-                return options["radius"](node).as<float>();
+                return options["radius"](node).as<double>();
             }
             return sqrt(node->value);
         };
 
-        std::function<float(HNode *)> paddingFunction = [this](HNode *node) {
+        std::function<double(HNode *)> paddingFunction = [this](HNode *node) {
             if(this->isType(options["padding"], "function"))
             {
-                return options["padding"](node).as<float>();
+                return options["padding"](node).as<double>();
             }
 
             return padding;
         };
 
-        std::function<float(HNode *)> zeroFn = [](HNode *node) { return 0; };
+        std::function<double(HNode *)> zeroFn = [](HNode *node) { return 0; };
 
         bool hasRadius = this->isType(options["radius"], "function") ||
-            this->isType(options["radius"], "float");
+            this->isType(options["radius"], "double");
 
         if(hasRadius) {
             root->eachBefore(radiusLeaf(radiusFunction));
@@ -401,29 +551,17 @@ Hierarchy* createHierarchy(emscripten::val options, emscripten::val node) {
 EMSCRIPTEN_BINDINGS(hierarchy) {
     register_vector<HNode*>("VectorHNode");
 
-    class_<HNode>("HNode")
+    class_<HBasis>("HBasis")
+        .property("x", &HBasis::x)
+        .property("y", &HBasis::y)
+        .property("r", &HBasis::r);
+
+    class_<HNode, base<HBasis>>("HNode")
         .constructor<emscripten::val>()
-        // .constructor<float>()
-        // .constructor<float, std::vector<HNode *>>()
         .property("children", &HNode::children)
-        // .property("parent", &HNode::getParent, &HNode::setParent)
         .property("value", &HNode::value);
-    // .property("x", &HNode::x)
-    // .property("y", &HNode::y)
-    // .property("r", &HNode::r)
-
-    // .function("eachBefore", &HNode::eachBefore)
-    // .function("eachAfter", &HNode::eachAfter);
-    // .function("doSomeStuff", &HNode::doSomeStuff);
-
-    // function("radiusLeaf", &radiusLeaf);
-    // function("packChildren", &packChildren);
-    // function("translateChild", &translateChild);
 
     class_<Hierarchy>("Hierarchy")
-        .constructor(&createHierarchy, allow_raw_pointers())
-
-        // .property("dx", &Hierarchy::dx)
-        // .property("dy", &Hierarchy::dy)
+        .constructor(&createHierarchy)
         .function("pack", &Hierarchy::pack, allow_raw_pointers());
 };
